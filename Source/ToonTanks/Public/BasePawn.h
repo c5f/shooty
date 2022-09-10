53617b127a4b5863a1a188939db4b4a05c86f763
colin <1137651+c5f@users.noreply.github.com>
2022-09-10 18:16:43 +1000

    expose unreal properties and setup blueprints
    - build blueprints for tank and tower (turret)
    
    learned about the keyboard shortcuts for the ortholinear viewports and
    how to use those views to align components within a blueprint
    
    there was also more content about uproperty parameters in order to
    expose values or properties in the blueprint event graph as well as
    metadata overrides and categorisation
    
    makes sense to chuck stuff under a project.system.feature categorisation
    for more complicated projects
    
    see: https://www.gamedev.tv/courses/1638644/lectures/39257425

--- Source/ToonTanks/Public/BasePawn.h
+++ Source/ToonTanks/Public/BasePawn.h
@@ -13,34 +13,33 @@ class TOONTANKS_API ABasePawn : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ABasePawn();
+
+	UPROPERTY(EditAnywhere, Category = "Shooty", BlueprintReadWrite)
+	float Speed = 400.0;
+
+	UPROPERTY(EditAnywhere, Category = "Shooty")
+	class UCapsuleComponent* Collider;
+
+	UPROPERTY(EditAnywhere, Category = "Shooty")
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(EditAnywhere, Category = "Shooty")
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(EditAnywhere, Category = "Shooty")
+	USceneComponent* ProjectileSpawnPoint;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-private:
-	UPROPERTY(EditAnywhere)
-	class UCapsuleComponent* Collider;
-
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* BaseMesh;
-
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* TurretMesh;
-
-	UPROPERTY(EditAnywhere)
-	USceneComponent* ProjectileSpawnPoint;
-
-	UPROPERTY(EditAnywhere)
-	float Speed = 400.0;
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	virtual void SetupPlayerInputComponent(
+		class UInputComponent* PlayerInputComponent
+	) override;
 };
