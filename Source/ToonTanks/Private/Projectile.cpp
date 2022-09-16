cfb0fd084a5cf5dad1016d0dcf0dfa0eaec5d18a
colin <1137651+c5f@users.noreply.github.com>
2022-09-16 18:10:15 +1000

    add health component module
    no logic -- just a bite-sized actor component with two editable private
    float members
    
    see: https://www.gamedev.tv/courses/1638644/lectures/39274752

--- Source/ToonTanks/Private/Projectile.cpp
+++ Source/ToonTanks/Private/Projectile.cpp
@@ -8,7 +8,8 @@
 // Sets default values
 AProjectile::AProjectile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Set this actor to call Tick() every frame.
+	// You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(
